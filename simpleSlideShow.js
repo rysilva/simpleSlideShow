ba8e4808c909c38adeda7b7c6f93dde3fe017ff7
Ryan Silva <rysilva@gmail.com>
2011-11-10 19:20:02 -0500

    Add script

--- simpleSlideShow.js
+++ simpleSlideShow.js
@@ -35,30 +35,48 @@
             images: []
         }, options);
 
-        var currImg = 0, images = settings.images;
+        var currImg = 0, images = settings.images, captionPadding = 15;
 
         if ( images.length < 2 ) {
             throw new Error("Error: Can't make a slideshow with less than 2 images.");
         }
         
+        //There are 2 images at a time on the page, one behind the other, both absolutely positioned
+        //To do the crossfade, image 1 fades in or out.  Image 2 is always at 100% opacity
         this.empty();
         var $2 = $('<img>').hide().appendTo(this),
             $1 = $('<img>').hide().appendTo(this),
             $caption = $('<div class="caption">').hide().appendTo(this);
 
+        $1.css('position', 'absolute');
+        $2.css('position', 'absolute');
+        $caption.css("background-color", "rgba(0,0,0,0.5)")
+                .css('color', '#eee')
+                .css('padding', '10px ' + captionPadding + 'px')
+                .css('position', 'relative')
+                .css('text-shadow', '#222 0 0 5px');
+
+
+        //Set the initial images
 	$2.attr('src', images[1]['src']).attr('caption', images[1]['caption']);
 	$1.attr('src', images[0]['src']).attr('caption', images[0]['caption'])
             .fadeIn(settings.transitionDelay, function() {
+                    //show the second one now, which is behind the first one
                     $2.show();
             });
-	$caption.text(images[0]['caption']);
+
+        if ( images[0]['caption'] ) {
+            $caption.text(images[0]['caption']);
+            $caption.css('width', ($1.outerWidth()-2*captionPadding) + 'px');
+        }
 				
+        //Show/hide caption
 	this.hover( 
             function(){
                     if ( $caption.is(":animated") ) {
                             $caption.stop();
                     }
-                    $caption.fadeTo(600, 1);
+                    $caption.fadeTo(600, $caption.text() ? 1 : 0 );
                     
             },
             function() {
@@ -69,6 +87,7 @@
             }
         );
 	
+        //Start the slideshow
 	setInterval(function() {
 		
 		//Hide the first image, show the second one.
@@ -77,9 +96,16 @@
 				function(idx){
 					return function() {
 						//Load next image #1
-						$1.attr('src', images[idx]['src']).attr('caption', images[idx]['caption']);
+                                                var caption = images[idx].caption || ""; 
+						$1.attr('src', images[idx]['src']).attr('caption', caption);
 						//Set caption on image 2
-						$caption.text(images[(idx+images.length-1)%images.length]['caption']);
+                                                caption = images[(idx+images.length-1)%images.length].caption || "";
+                                                $caption.text(caption);
+                                                $caption.css('width', ($2.outerWidth()-2*captionPadding) + 'px');
+                                                //If there is no caption, hide it
+                                                if ( !caption ) {
+                                                    $caption.fadeTo(400,0);
+                                                }
 					}
 				}((currImg+2)%images.length)
 			);
@@ -89,8 +115,14 @@
 			$1.fadeIn(settings.transitionDelay,
 				function(idx) {
 					return function() {
-						$2.attr('src', images[idx]['src']).attr('caption', images[idx]['caption']);
-						$caption.text(images[(idx+images.length-1)%images.length]['caption']);
+                                                var caption = images[idx].caption || "";
+						$2.attr('src', images[idx]['src']).attr(caption);
+                                                caption = images[(idx+images.length-1)%images.length].caption || "";
+                                                $caption.text(caption);
+                                                $caption.css('width', ($1.outerWidth()-2*captionPadding) + 'px');
+                                                if ( !caption ) { 
+                                                    $caption.fadeTo(400,0);
+                                                }
 					}
 				}((currImg+2)%images.length)
 			);
